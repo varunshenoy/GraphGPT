b602b7df31b88637dec3bb77b9d624faa12247ce
Varun Shenoy <varun.nshenoy@gmail.com>
2023-01-31 23:21:35 -0800

    added textfield for apikey

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import './App.css';
 import Graph from "react-graph-vis";
 import React, { useState } from "react";
 
-if (!localStorage.getItem("OPENAI_API_KEY")) {
-  localStorage.setItem("OPENAI_API_KEY", prompt("Enter your OpenAI API key"));
-}
-
-const OPENAI_API_KEY = localStorage.getItem("OPENAI_API_KEY");
-
 const DEFAULT_PARAMS = {
   "model": "text-davinci-003",
   "temperature": 0.3,
@@ -47,7 +41,7 @@ function App() {
     })
   }
 
-  const queryPrompt = (prompt) => {
+  const queryPrompt = (prompt, apiKey) => {
     fetch('prompts/main.prompt')
       .then(response => response.text())
       .then(text => text.replace("$prompt", prompt))
@@ -61,7 +55,7 @@ function App() {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + String(OPENAI_API_KEY)
+            'Authorization': 'Bearer ' + String(apiKey)
           },
           body: JSON.stringify(params)
         };
@@ -81,6 +75,7 @@ function App() {
             document.getElementsByClassName("searchBar")[0].value = "";
           }).catch(error => {
             console.log(error);
+            alert("Error: " + error + ". Please check your API key.");
             document.body.style.cursor = 'default';
             document.getElementsByClassName("generateButton")[0].disabled = false;
           });
@@ -89,22 +84,24 @@ function App() {
 
 
   const createGraph = () => {
-    // document.body.style.cursor = 'wait';
+    document.body.style.cursor = 'wait';
 
     document.getElementsByClassName("generateButton")[0].disabled = true;
     const prompt = document.getElementsByClassName("searchBar")[0].value;
+    const apiKey = document.getElementsByClassName("apiKeyTextField")[0].value;
 
-    queryPrompt(prompt);
+    queryPrompt(prompt, apiKey);
   }
 
   return (
     <div className='container'>
       <h1 className="headerText">GraphGPT 🔎</h1>
       <p className='subheaderText'>Build complex, directed graphs to add structure to your ideas using natural language. Understand the relationships between people, systems, and maybe solve a mystery.</p>
-
+      <p className='opensourceText'><a href="https://github.com/varunshenoy/graphgpt">GraphGPT is open-source</a>&nbsp;🎉</p>
       <center>
         <div className='inputContainer'>
           <input className="searchBar" placeholder="Describe your graph..."></input>
+          <input className="apiKeyTextField" type="password" placeholder="Enter your OpenAI API key..."></input>
           <button className="generateButton" onClick={createGraph}>Generate</button>
           <button className="clearButton" onClick={clearState}>Clear</button>
         </div>
